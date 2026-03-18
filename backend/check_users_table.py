8af5a81aa2bb70f04a60c052bdaf550dd12fe76f
anishghanwat <anishghanwat.work@gmail.com>
2026-03-18 16:06:50 +0530

    cleanup: remove 60+ one-off scripts, debug files, output txts, unused mockData, scripts folder

--- backend/check_users_table.py
+++
@@ -1,6 +0,0 @@
-from database import engine
-from sqlalchemy import text
-
-with engine.connect() as conn:
-    result = conn.execute(text('SHOW CREATE TABLE users'))
-    print(result.fetchone()[1])
